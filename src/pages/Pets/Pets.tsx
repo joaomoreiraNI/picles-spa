0b707e0f8ce7a68db8c816e116957ee43e2000d5
Joao <joaomoreira@agger.com.br>
2024-04-22 19:08:20 -0300

    Create IFilterColumns

--- src/pages/Pets/Pets.tsx
+++ src/pages/Pets/Pets.tsx
@@ -1,25 +1,28 @@
-import { useQuery } from "@tanstack/react-query";
+import { useSearchParams } from "react-router-dom";
 import { Card } from "../../components/common/Card";
 import { Header } from "../../components/common/Header";
-import { Grid } from "../../components/layout/Grid";
-import { getPets } from "../../services/pets/getPets";
-import styles from "./Pets.module.css";
-import { Skeleton } from "../../components/common/Skeleton";
 import { Pagination } from "../../components/common/Pagination";
-import { useSearchParams } from "react-router-dom";
+import { Skeleton } from "../../components/common/Skeleton";
+import { Grid } from "../../components/layout/Grid";
+import { usePetList } from "../../hooks/usePetList";
+import styles from "./Pets.module.css";
+import { Select } from "../../components/common/Select";
+import { Button } from "../../components/common/Button";
+import { filterColumns } from "./Pets.constants";
+import { FormEvent } from "react";
+import { GetPetsRequest } from "../../interfaces/pet";
 
 export function Pets() {
   const [searchParams, setSearchParams] = useSearchParams();
 
   const urlParams = {
     page: searchParams.get("page") ? Number(searchParams.get("page")) : 1,
+    type: searchParams.get("type") ?? "",
+    size: searchParams.get("size") ?? "",
+    gender: searchParams.get("gender") ?? "",
   };
 
-  const { data, isLoading } = useQuery({
-    queryKey: ["get-pets", urlParams],
-    queryFn: () => getPets(urlParams),
-    staleTime: 100,
-  });
+  const { data, isLoading } = usePetList(urlParams);
 
   function changePage(page: number) {
     setSearchParams((params) => {
@@ -28,33 +31,74 @@ export function Pets() {
     });
   }
 
+  function getFormValue(form: HTMLFormElement) {
+    const formData = new FormData(form);
+    return Object.fromEntries(formData);
+  }
+
+  function updateSearchParams(urlParams: GetPetsRequest) {
+    const fields: (keyof GetPetsRequest)[] = ["type", "size", "gender"];
+    const newParams = new URLSearchParams();
+
+    fields.forEach((field) => {
+      if (urlParams[field]) {
+        newParams.set(field, String(urlParams[field]));
+      }
+    });
+    newParams.set("page", "1");
+
+    return newParams;
+  }
+
+  function applyFilters(event: FormEvent) {
+    event.preventDefault();
+
+    const formValues = getFormValue(event.target as HTMLFormElement);
+    const newSearchParams = updateSearchParams(formValues);
+
+    setSearchParams(newSearchParams);
+  }
+
   return (
-    <>
-      <Grid>
-        <div className={styles.container}>
-          <Header />
-          {isLoading && (
-            <Skeleton containerClassName={styles.skeleton} count={10} />
-          )}
-          <main className={styles.list}>
-            {data?.items?.map((pet) => (
-              <Card
-                key={pet.id}
-                href={`/pet/${pet.id}`}
-                text={pet.name}
-                thumb={pet.photo}
-              />
+    <Grid>
+      <div className={styles.container}>
+        <Header />
+        <form className={styles.filters} onSubmit={applyFilters}>
+          <div className={styles.columns}>
+            {filterColumns.map((filter) => (
+              <div key={filter.name} className={styles.column}>
+                <Select
+                  label={filter.title}
+                  defaultValue={urlParams[filter.name]}
+                  name={filter.name}
+                  options={filter.options}
+                />
+              </div>
             ))}
-          </main>
-          {data?.currentPage && (
-            <Pagination
-              currentPage={data.currentPage}
-              totalPages={data.totalPages}
-              onPageChange={(number) => changePage(number)}
+          </div>
+          <Button type="submit">Buscar</Button>
+        </form>
+        {isLoading && (
+          <Skeleton containerClassName={styles.skeleton} count={10} />
+        )}
+        <main className={styles.list}>
+          {data?.items?.map((pet) => (
+            <Card
+              key={pet.id}
+              href={`/pet/${pet.id}`}
+              text={pet.name}
+              thumb={pet.photo}
             />
-          )}
-        </div>
-      </Grid>
-    </>
+          ))}
+        </main>
+        {data?.currentPage && (
+          <Pagination
+            currentPage={data.currentPage}
+            totalPages={data.totalPages}
+            onPageChange={(number) => changePage(number)}
+          />
+        )}
+      </div>
+    </Grid>
   );
 }
