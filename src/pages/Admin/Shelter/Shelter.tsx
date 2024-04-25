7a1f069d7d2fdf1b08af6704472d20a710a5f130
Joao <joaomoreira@agger.com.br>
2024-04-24 21:22:41 -0300

    Criação de validação do form

--- src/pages/Admin/Shelter/Shelter.tsx
+++ src/pages/Admin/Shelter/Shelter.tsx
@@ -1,14 +1,82 @@
 import styles from "./Shelter.module.css";
-
 import { Button } from "../../../components/common/Button";
 import { Input } from "../../../components/common/Input";
 import { Panel } from "../../../components/layout/Panel";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useHookFormMask } from "use-mask-input";
+
+const shelterSchema = z.object({
+  name: z
+    .string()
+    .min(2, "Nome deve ter no mínimo 2 caracteres.")
+    .max(30, "Nome deve ter no máximo 30 caracteres"),
+  email: z.string().email("Campo deve ser um e-mail"),
+  phone: z.string().refine(
+    (value) => {
+      const digits = value.replace(/\D/g, "").length;
+      return digits >= 10 && digits <= 13;
+    },
+    { message: "Número deve ter entre 10 e 13 caracteres" }
+  ),
+  whatsApp: z.string().refine(
+    (value) => {
+      const digits = value.replace(/\D/g, "").length;
+      return digits >= 10 && digits <= 13;
+    },
+    { message: "Número deve ter entre 10 e 13 caracteres" }
+  ),
+});
+
+type ShelterSchema = z.infer<typeof shelterSchema>;
 
 export function Shelter() {
+  const { handleSubmit, register, formState } = useForm<ShelterSchema>({
+    resolver: zodResolver(shelterSchema),
+  });
+
+  const registerWhithMask = useHookFormMask(register);
+
+  function submit({ name, email, phone, whatsApp }: ShelterSchema) {
+    console.log(name, email, phone, whatsApp);
+  }
   return (
     <Panel>
-      <form className={styles.container}>
-        <Input label="None" value="" className="" />
+      <form className={styles.container} onSubmit={handleSubmit(submit)}>
+        <div>
+          <Input label="Nome" {...register("name")} />
+          {formState.errors?.name && (
+            <p className={styles.formError}>{formState.errors.name.message}</p>
+          )}
+        </div>
+        <div>
+          <Input label="Email" {...register("email")} />
+          {formState.errors?.email && (
+            <p className={styles.formError}>{formState.errors.email.message}</p>
+          )}
+        </div>
+        <div>
+          <Input
+            label="Telefone"
+            {...registerWhithMask("phone", ["99 9999-9999"])}
+          />
+          {formState.errors?.phone && (
+            <p className={styles.formError}>{formState.errors.phone.message}</p>
+          )}
+        </div>
+        <div>
+          <Input
+            label="WhatsApp"
+            {...registerWhithMask("whatsApp", ["99 99999-9999"])}
+          />
+          {formState.errors?.whatsApp && (
+            <p className={styles.formError}>
+              {formState.errors.whatsApp.message}
+            </p>
+          )}
+        </div>
+
         <Button type="submit">Salvar dados</Button>
       </form>
     </Panel>
